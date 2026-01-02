2afc5ac556c3b063ca72bad769b802b2a271cb6c
Dan Best <dilshoddilmurodov799@gmail.com>
2026-01-03 00:52:36 +0500

    bugs fixed

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ import { app, server } from './lib/socket.js'
 
 const __dirname = path.resolve()
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 5000; 
 
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
 app.use(express.json({limit: "20mb"})) // req.body
