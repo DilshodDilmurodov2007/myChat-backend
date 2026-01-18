57886f98677531e383b48a9aeff1f6bb15912d46
Dan Best <dilshoddilmurodov799@gmail.com>
2026-01-18 23:10:16 +0500

    Finally finally fixed!

--- src/server.js
+++ src/server.js
@@ -25,15 +25,13 @@ app.use("/api/messages", messageRoutes)
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
-    app.use(
-        express.static(
-            path.join(__dirname, "../frontend/dist")
-        )
-    )
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
-    })
-} 
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Catch-all: send React/Vite app for any non-API route
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
     console.log("Server running on port "+ PORT)
