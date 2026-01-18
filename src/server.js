9bde97477fd50cb6af53555b8ba8b4643ee8327b
Dan Best <dilshoddilmurodov799@gmail.com>
2026-01-18 23:12:49 +0500

    FIXED!

--- src/server.js
+++ src/server.js
@@ -28,9 +28,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Catch-all: send React/Vite app for any non-API route
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+//   app.get(/.*/, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
 }
 
 server.listen(PORT, () => {
